af579bf41b54c2fe1d4f9530fb884cbdcb554bd3
pi3 <pi3@raspberry.net>
2016-11-27 22:02:34 +0000

    created custom service to get city

--- app.js
+++ app.js
@@ -12,10 +12,17 @@ weatherApp.config(function($routeProvider) {
 		})
 });
 
-weatherApp.controller('homeController', ['$scope', function($scope) {
-	
+weatherApp.service('cityService', function() {
+	this.city = "Miami, FL";
+});
+
+weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
+	$scope.$watch('city', function() {
+		cityService.city = $scope.city;
+	});
 }]);
 
-weatherApp.controller('forecastController', ['$scope', function($scope) {
-	
+weatherApp.controller('forecastController', ['$scope', 'cityService', function($scope, cityService) {
+	$scope.city = cityService.city;
 }]);
