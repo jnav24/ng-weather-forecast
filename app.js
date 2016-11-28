1f272896969666bdce0739717ced07c969eea281
pi3 <pi3@raspberry.net>
2016-11-28 00:10:13 +0000

    made search results into a directive

--- app.js
+++ app.js
@@ -20,6 +20,20 @@ weatherApp.service('cityService', function() {
 	this.city = "Miami, FL";
 });
 
+weatherApp.directive('searchResults', function() {
+	return {
+		replace: true,
+		restrict: 'E',
+		templateUrl: 'templates/results.html',
+		scope: {
+			convertToStandard: "&",
+			convertToDate: "&",
+			dateFormat: "@",
+			weatherDay: "="
+		}
+	};
+});
+
 weatherApp.controller('homeController', ['$scope', 'cityService', function($scope, cityService) {
 	$scope.city = cityService.city;
 	$scope.$watch('city', function() {
@@ -28,12 +42,12 @@ weatherApp.controller('homeController', ['$scope', 'cityService', function($scop
 }]);
 
 weatherApp.controller('forecastController', ['$scope', '$resource', 'cityService', '$routeParams', function($scope, $resource, cityService, $routeParams) {
-	$scope.days = ($routeParams.days || 1) * 8;
+	$scope.days = ($routeParams.days || 1);
 	$scope.city = cityService.city;
 	$scope.weatherApi = $resource("http://api.openweathermap.org/data/2.5/forecast", 
 		{ callback: "JSON_CALLBACK" },
 		{ get: {method: "JSONP"}});
-	$scope.weatherResult = $scope.weatherApi.get({ q: $scope.city, appid: weather_api_key, cnt: $scope.days });
+	$scope.weatherResult = $scope.weatherApi.get({ q: $scope.city, appid: weather_api_key, cnt: ($scope.days* 8) });
 
 	$scope.convertToFahrenheit = function(degK) {
 		return Math.round( (1.8*(degK-273)+32) );
